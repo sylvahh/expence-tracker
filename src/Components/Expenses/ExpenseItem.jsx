af59eae1795cdf5cbaf3f6ded4207fcca246508f
sylvahh <sylvesterchambers03@gmail.com>
2023-08-05 12:38:18 +0100

    change the currency sign

--- src/Components/Expenses/ExpenseItem.jsx
+++ src/Components/Expenses/ExpenseItem.jsx
@@ -10,7 +10,7 @@ const ExpenseItem = (props) => {
           <div className="expense-item__description">
               <h2>{props.title}</h2>
               <div className="expense-item__price">
-                  ${props.price}
+              ₦{props.price}
               </div>
           </div>
       </li>
